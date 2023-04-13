a1626ac74193261b7bbcd85d3956c08ff135173e
eceuykur <uykurece@gmail.com>
2023-04-13 21:15:27 +0300

    kafka

---
+++ src/main/java/tr/nttdata/poc/minicommerce/kafkalistener/model/KafkaLog.java
@@ -0,0 +1,25 @@
+package tr.nttdata.poc.minicommerce.kafkalistener.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.io.Serializable;
+import java.util.Date;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Document(collection = "logs")
+public class KafkaLog implements Serializable {
+    @Id
+    private String id;
+    private String user;
+    private Double action;
+    private Date textInfo;
+    private String parameters;
+}
---
+++ src/main/java/tr/nttdata/poc/minicommerce/kafkalistener/properties/ConsumerApplicationProperties.java
@@ -0,0 +1,15 @@
+package tr.nttdata.poc.minicommerce.kafkalistener.properties;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Data
+@Configuration
+@ConfigurationProperties(prefix = "kafka")
+@EnableConfigurationProperties
+public class ConsumerApplicationProperties {
+    private String topic;
+    private int messages;
+}
