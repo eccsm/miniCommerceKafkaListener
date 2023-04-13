9c4839bbfc5669ac8aaac16e049f5c70e6a40e27
eccsm <eccsm34@gmail.com>
2023-04-13 17:23:54 +0300

    first commit

---
+++ src/test/java/tr/nttdata/poc/minicommerce/kafkalistener/KafkaListenerApplicationTests.java
@@ -0,0 +1,13 @@
+package tr.nttdata.poc.minicommerce.kafkalistener;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaListenerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
